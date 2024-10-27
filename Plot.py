580a1cfe6bab75d41a0de759563c6422c135cbf5
Theodor Henningsen <th@math.ku.dk>
2024-10-27 18:34:58 +0100

    plotting functionality update

--- Plot.py
+++ Plot.py
@@ -1,19 +1,29 @@
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from HillEstimator import HillEstimator
 class Plot:
-    def __init__(self, hill_estimate_data):
-        self.hill_estimate_data = hill_estimate_data
-        self.x     = (self.hill_estimate_data)['X']
-        self.k     = (self.hill_estimate_data)['K']
-        self.gamma = (self.hill_estimate_data)['gamma_k_x']
-        self.three_d = self.plot_3d
-
-
+    def __init__(self, user_input):
+        self.user_input = user_input
+        self.hill_estimator = HillEstimator(user_input = user_input)
+        self.hill_estimator.estimate()
+        self.x       = self.hill_estimator.hill_estimate['X']
+        self.k       = self.hill_estimator.hill_estimate['K']
+        self.gamma   = self.hill_estimator.hill_estimate['gamma_k_x']
+        self.three_d = self.plot_3d()
+        self.scatter = self.plot_2d()
 
     def plot_2d(self):
-        pass
+        dates = self.user_input.get_time()
+        x = self.user_input.get_covariate_process()
+        y = self.user_input.get_rv_process()
+        ts_plot = make_subplots(rows=2, cols=1, subplot_titles=("covariate process", "rv process"))
+        ts_plot.add_trace(go.Scatter(x = dates, y = x), row=1, col=1)
+        ts_plot.add_trace(go.Scatter(x = dates, y = y), row=2, col=1)
+        #ts_plot.show()
+        return ts_plot
 
     def plot_3d(self):
-        fig = go.Figure(data=[go.Surface(z=gamma, x=x, y=k)])
+        fig = go.Figure(data=[go.Surface(z = self.gamma, x = self.x, y = self.k)])
         fig.update_layout(title='gamma_k_x', autosize=True)
         return fig
         #fig.show()
--- conditional_hill_estimator.py
+++ conditional_hill_estimator.py
@@ -1,13 +1,10 @@
 import sys
 from Plot import Plot
-from HillEstimator import HillEstimator
 from parser import parse_command_line_arguments
 
 def main():
         user_input = parse_command_line_arguments(sys.argv)
-        hill_estimator = HillEstimator(user_input = user_input)
-        hill_estimator.estimate()
-        plot = Plot(hill_estimate_data = hill_estimator.hill_estimate)
+        plot = Plot(user_input = user_input)
         plot.plot_2d()
         plot.plot_3d()
         # hill_plots(estimator)
