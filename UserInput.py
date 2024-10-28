06d3122e204902df88337f74e34f02e0a68accbe
Sigmundur Hannesarson <sigmundur.hannesarson@gmail.com>
2024-10-28 20:10:26 +0800

    user input

--- UserInput.py
+++ UserInput.py
@@ -1,7 +1,11 @@
 class UserInput:
-    def __init__(self, time_series, kernel):
+    def __init__(self, stock_tickers=None, transform_type=None, time_series=None):
+        self.stock_tickers = stock_tickers
         self.time_series = time_series
-        self.kernel = kernel
+        self.transform_type = transform_type
+        self.time_series_transformed = None
+        self.time_series_transformed_positive = None
+        self.time_series_transformed_negative = None
 
     def get_time(self):
         return self.time_series['date']
@@ -11,3 +15,15 @@ class UserInput:
 
     def get_rv_process(self):
         return self.time_series['rv']
+
+    def transform_time_series(self):
+        # implement
+        # self.time_series_transformed = ...
+        pass
+
+    def split_time_series(self):
+        # self.time_series_transformed_positive = ..
+        # self.time_series_transformed_negative =
+        pass
+
+
